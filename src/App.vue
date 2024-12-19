d14fa03d4f925caaab3feeffa4b8c72ca0c13b11
DavidO28 <damers22@yahoo.com>
2024-12-19 19:37:57 +0400

    added prettier for code quality

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,5 @@
 </template>
 
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+  import { RouterView } from 'vue-router'
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1>Finance tracker</h1>
+  <h1>Finance tracker</h1>
 </template>
