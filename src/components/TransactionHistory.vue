3030da9bd687e4c9c88c81144c3eb08f28a200cb
DavidO28 <damers22@yahoo.com>
2024-12-24 14:02:42 +0400

    adjusted styles

--- src/components/TransactionHistory.vue
+++ src/components/TransactionHistory.vue
@@ -3,19 +3,19 @@
     <v-card class="pa-7">
       <h3 class="mb-3">Transaction history</h3>
       <v-list>
-        <v-list-item
-          v-for="transaction in sortedTransactions"
-          :style="{ color: transaction.amount < 0 ? '#FF0000' : '#40FF40' }"
-        >
-          <span class="d-flex flex-row align-center justify-center">
+        <v-list-item v-for="transaction in sortedTransactions">
+          <span
+            :style="{ color: transaction.amount < 0 ? '#FF0000' : '#40FF40' }"
+          >
             {{ transaction.text }} ${{ transaction.amount }}
-            <v-icon
-              class="ms-3"
-              @click="transactionStore.deleteTransaction(transaction.id)"
-            >
-              mdi-delete
-            </v-icon>
           </span>
+          <v-icon
+            class="ms-3 mb-1"
+            size="small"
+            @click="transactionStore.deleteTransaction(transaction.id)"
+          >
+            mdi-delete
+          </v-icon>
         </v-list-item>
       </v-list>
     </v-card>
