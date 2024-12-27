1a853f983857facedc98547e1d3f278d91039940
DavidO28 <damers22@yahoo.com>
2024-12-27 17:33:18 +0400

    configured to deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory('finance-app'),
+  history: createWebHistory('/finance-app/'),
   routes: [
     {
       path: '/',
